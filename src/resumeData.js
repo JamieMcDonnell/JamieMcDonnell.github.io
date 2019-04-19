d29f4a717d40e42d215c0b85e098860449d6369b
JamieMcDonnell <jamie.mcdonnell@gmail.com>
2019-04-19 13:38:16 +0200

    deleting contents

--- src/resumeData.js
+++
@@ -1,6 +0,0 @@
-let resumeData = {
-  "imagebaseurl":"http://localhost:3000/",
-  "name": "Jamie McDonnell",
-  "role": "Creative Prague based front end UI designer and javascript deveoper with 19 years of work experience developing cutting edge user experiences for big brands, startups and everything inbetween."
-}
-export default resumeData
